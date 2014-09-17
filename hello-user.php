2aa777271db8da9fe8a0b9c5e757b9ca2b90aa09
Peter Suhm <peter@suhm.dk>
2014-09-17 10:47:34 +0700

    initial commit

---
+++ hello-user.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: Hello User
+ * Author: WP Pusher
+ */
+
+add_filter( "wp_loaded", "checkForGreeting" );
+
+function checkForGreeting()
+{
+    if (isset($_GET['hello'])) {
+
+        echo "Hello " . $_GET['hello'] . "!";
+        die();
+
+    }
+}
